5cd9d6cfd884ef11386faaac5074895d531f9c11
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-02-20 22:25:48 -0600

    Functioning diggability detection

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+local function is_node_vein_diggable(nodeName, current_tool)
+    local caps = current_tool:get_tool_capabilities()
+    local nodeGroups = minetest.registered_nodes[nodeName].groups
+    local test = minetest.get_dig_params(nodeGroups, caps)
+
+    return test["diggable"]
+end
+
 minetest.register_on_dignode(function(pos, node, digger)
-    minetest.debug("Dug node")
+    local current_tool = digger:get_wielded_item()
+    minetest.debug(current_tool:get_name())
+    if is_node_vein_diggable(node.name, current_tool) then
+        minetest.debug("Diggable")
+    end
 end)
