2cbc8537c1e0e18626e046b0452abbe422f2837a
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-08-12 12:50:53 -0500

    Possibly unessecary diggable function

--- init.lua
+++ init.lua
@@ -1,49 +1,28 @@
 vein_miner = {}
-vein_miner.ore_content_ids = {}
-vein_miner.stone_content_ids = {}
 
-local function is_node_vein_diggable(nodeName)
+local function is_node_vein_diggable(nodeName, current_tool)
     -- -- Get the current tools capabilities
-    -- local caps = current_tool:get_tool_capabilities()
+    local caps = current_tool:get_tool_capabilities()
     -- -- Get the groups for the current node
-    -- local nodeGroups = minetest.registered_nodes[nodeName].groups
+    local nodeGroups = minetest.registered_nodes[nodeName].groups
     -- -- Test if the current tool can dig the node
-    -- local test = minetest.get_dig_params(nodeGroups, caps)
+    local test = minetest.get_dig_params(nodeGroups, caps)
     -- -- Return if the node is diggable
+    return test.diggable
+end
 
-    -- Check if the mined node is a registered ore
-    local node_id = minetest.get_content_id(nodeName)
-    if vein_miner.ore_content_ids[node_id] then 
-        return true 
-    else 
-        return false 
-    end
+local function dig_pos(pos)
+
 end
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
     local current_tool = digger:get_wielded_item()
     minetest.debug(current_tool:get_name())
-    if is_node_vein_diggable(oldnode.name) then
-        minetest.debug("Diggable")
-    end
+    --if is_node_vein_diggable(oldnode.name, current_tool) then
+    --    minetest.debug("Diggable")
+    --end
+
+    
 end)
 
--- From ore cutting mod
--- https://github.com/minetest-mods/orecutting/blob/master/init.lua
-minetest.after(0, function ()
-	for _, ore in pairs(minetest.registered_ores) do
-		if ore.wherein and ore.ore_type == "scatter" then
-			local id = minetest.get_content_id(ore.ore)
-			vein_miner.ore_content_ids[id] = ore.ore
-			if type(ore.wherein) == "table" then
-				for _, v in ipairs(ore.wherein) do
-					id = minetest.get_content_id(v)
-					vein_miner.stone_content_ids[id] = v
-				end
-			else
-				id = minetest.get_content_id(ore.wherein)
-				vein_miner.stone_content_ids[id] = ore.wherein
-			end
-		end
-	end
-end)
+
