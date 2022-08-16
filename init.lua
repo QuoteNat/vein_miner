60921e2801f6cd06cce6be178a5bb0172eeab79e
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-08-16 18:55:35 -0500

    Fixed a bug where MAX_MINED_NODES is nil

--- init.lua
+++ init.lua
@@ -21,7 +21,15 @@ minetest.register_on_mods_loaded(function()
       -- Get settings
 
       -- Fetch settings
-      MAX_MINED_NODES = tonumber(minetest.settings:get("max_nodes"))
+      MAX_MINED_NODES = tonumber(minetest.settings:get("vein_miner_max_nodes"))
+
+      -- Set MAX_MINED_NODES to default value in case getting the setting doesn't work
+      if MAX_MINED_NODES == nil then
+	 MAX_MINED_NODES = 188
+      end
+      
+      minetest.debug(MAX_MINED_NODES)
+      minetest.debug(minetest.settings:get("vein_miner_max_nodes"))
       local stringtoboolean = { ["true"]=true, ["false"]=false }
       local allow_ores = stringtoboolean[minetest.settings:get("allow_ores")]
       local allow_trees = stringtoboolean[minetest.settings:get("allow_trees")]
