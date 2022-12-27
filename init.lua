98baeadcf8229c7b4f093badb3fa2911796fd0d7
Awkanimus <awkanimus@thenightstalkers.org>
2022-12-26 22:48:05 -0500

    Use table as set to reduce search from O(n) to O(log2(n))

--- init.lua
+++ init.lua
@@ -33,7 +33,7 @@ minetest.register_on_mods_loaded(function()
       local allow_all   = minetest.settings:get_bool("allow_all", false)
       -- Initialize tool whitelist with registered tools
       for name, def in pairs(minetest.registered_tools) do
-	 table.insert(rTools, name)
+          rTools[def.name] = true
       end
       
       -- Initialize whitelist for registered nodes
@@ -48,48 +48,41 @@ minetest.register_on_mods_loaded(function()
 	    for name, def in pairs(minetest.registered_ores) do
 	       local node_name = def.ore
 	       if string.find(node_name, "stone_with_") ~= nil then
-		  table.insert(rNodes, node_name)
+               rNodes[node_name] = true
 	       end
 	    end
 	 end
 
 	 -- Register tree nodes
-	 if allow_trees then
-	    for name, def in pairs(minetest.registered_nodes) do
-	       if def.groups.tree ~= nil then
-		  local node_name = def.name
-		  table.insert(rNodes, node_name)
-	       end
-	    end
-	 end
+	if allow_trees then
+		for name, def in pairs(minetest.registered_nodes) do
+			if def.groups.tree ~= nil then
+				local node_name = def.name
+				rNodes[node_name] = true
+			end
+		end
+	end
       end
 end)
 
 
 local function is_node_vein_diggable(nodeName, wieldedName)
-   local nodeCheck = nodeBlacklist
-   local toolCheck = toolBlacklist
-   -- check nodes
-   for k, v in pairs(rNodes) do
-      if v == nodeName and nodeBlacklist == true then
-	 nodeCheck = false
-      elseif v == nodeName and nodeBlacklist == false then
-	 nodeCheck = true	
-      end
-   end
+    local nodeCheck = nodeBlacklist and true or false
+    local toolCheck = toolBlacklist and true or false
 
-   -- return false if nodeCheck failed
-   --if nodeCheck == false then return false end	
-   
-   for k, v in pairs(rTools) do
-      if v == wieldedName and toolBlacklist == true then
-	 toolCheck = false
-      elseif v == wieldedName and toolBlacklist == false then
-	 toolCheck = true	
-      end
-   end
-   
-   return nodeCheck and toolCheck
+    -- check nodes
+    if next(rNodes) then
+        local isNodeInRnodes = (rNodes[nodeName] ~= nil) and true or false
+        nodeCheck = (isNodeInRnodes ~= nodeBlacklist)
+    end
+
+    -- check tools
+    if next(rTools) then
+        local isToolInRtools = (rTools[wieldedName] ~= nil) and true or false
+        toolCheck = (isToolInRtools ~= toolBlacklist)
+    end
+
+    return nodeCheck and toolCheck
 end
 
 -- Recursively mines a vein of blocks
